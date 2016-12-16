ded74d7f9f42969ca217b5a7456d000f283e341e
Ken Dale <ken@kendaleiv.com>
2016-12-15 19:59:07 -0500

    Include 32-bit and 64-bit versions, version package to 1.1.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿$packageName = "postman"
 $fileType = "exe"
 $silentArgs = "-s"
-$url = "https://dl.pstmn.io/download/latest/win?arch=64"
+$url = "https://dl.pstmn.io/download/latest/win32"
+$url64 = "https://dl.pstmn.io/download/latest/win64"
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
