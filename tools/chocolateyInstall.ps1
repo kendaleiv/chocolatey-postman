881ea7f2b323b2fce94a484ee53ff1bb1c5e00ab
Eugene Baranov <220614+eugbaranov@users.noreply.github.com>
2018-11-14 21:10:00 +0000

    6.5.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,19 @@
-﻿$packageName = "postman"
+$packageName = "postman"
 $fileType = "exe"
 $silentArgs = "-s"
-$url = "https://dl.pstmn.io/download/latest/win32"
-$url64 = "https://dl.pstmn.io/download/latest/win64"
+$url = "https://dl.pstmn.io/download/version/6.5.2/windows32"
+$url64 = "https://dl.pstmn.io/download/version/6.5.2/windows64"
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = "exe"
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "-s"
+  checksum      = 'E210ECE30C21205486E0328F52C20E7A932C0D7CBFA8642022602A4C9F597F62'
+  checksumType  = 'sha256'
+  checksum64    = '0FD841E28B6E1F9183155B54CA70C81E108436871F98CE5834794DD4B8B042C2'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
