acf2a9fe248a7b4db23afb1d36850a86c6edb8d4
leander091 <leander091@users.noreply.github.com>
2019-08-30 18:38:52 +0200

    New release  7.6.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName= 'postman'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.pstmn.io/download/version/7.5.0/windows32'
-$url64      = 'https://dl.pstmn.io/download/version/7.5.0/windows64'
+$url        = 'https://dl.pstmn.io/download/version/7.6.0/windows32'
+$url64      = 'https://dl.pstmn.io/download/version/7.6.0/windows64'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -9,9 +9,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   silentArgs    = "-s"
-  checksum      = '38DEA98BA705F454AD5F23521E074D1B05F08A534684B706A27C8508227D9318'
+  checksum      = 'AFDDAF0FD8FDF19A4EEA9F08D7105A0AAA9B6DD5408E72CE6D48462A737546FE'
   checksumType  = 'sha256'
-  checksum64    = '30BF13EE1609E2027028074B4A4509CB564507BB058640A34EFE5A1D45E03BF5'
+  checksum64    = 'EB1A29D69F45F0D6524CD4047FBE958E3A7E7B7229CB2DF72FB259C74E0C496C'
   checksumType64= 'sha256'
 }
 
