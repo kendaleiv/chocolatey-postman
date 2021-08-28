d2dc0d25ee3f9ad98a1167836d895620715831e9
Ken Dale <ken@kendaleiv.com>
2021-08-27 20:23:03 -0400

    8.11.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName= 'postman'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://dl.pstmn.io/download/version/8.9.1/windows32'
-$url64      = 'https://dl.pstmn.io/download/version/8.9.1/windows64'
+$url        = 'https://dl.pstmn.io/download/version/8.11.1/windows32'
+$url64      = 'https://dl.pstmn.io/download/version/8.11.1/windows64'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -9,9 +9,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   silentArgs    = "-s"
-  checksum      = 'BFAF003137DE67B7066104A8D4E58F56ED0239A12ADB01DBAFBC45B492B87F37'
+  checksum      = 'FA9F5F631C4777AC0F1B6D61EC7AEBCDCF1B55CAFAE96EB51BDA49BB52635176'
   checksumType  = 'sha256'
-  checksum64    = 'BA99BD71F8EF1A3777058414A343CCEF1F1F5548FE06383272C9D4D42B67C336'
+  checksum64    = '76AD2A504764D447C948F6DDE8A7BC64582AAA4E0E0ED27D380154B901B7F0D5'
   checksumType64= 'sha256'
 }
 
