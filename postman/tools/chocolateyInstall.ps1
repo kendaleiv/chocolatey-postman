a5d0091184945ff7f92d3a3940ee6a7b805b93ba
manavbharambe <manav.bharambe@postman.com>
2023-02-16 19:09:56 +0530

    Upgraded to 10.10.3

--- postman/tools/chocolateyInstall.ps1
+++ postman/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName= 'postman'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://dl.pstmn.io/download/version/10.9.4/win64'
+$url64      = 'https://dl.pstmn.io/download/version/10.10.3/win64'
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
   silentArgs    = "-s"
   url64bit      = $url64
-  checksum64    = 'f164eed1e7cd289f256a79230a1531321b979dc59385af7b2a34ba3c6904f5eb'
+  checksum64    = '6ae0453eeb9d95e36c0fb02c40558a14330a3093384f0ac27ef185d015396041'
   checksumType64= 'sha256'
 }
 
