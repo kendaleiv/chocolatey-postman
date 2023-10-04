d5e5aacb1fc93be1e3e96de1da74bedbadbef6ad
Zoull <zoull@zdindustries.com>
2023-10-04 12:45:27 -0600

    Update Postman to version 10.18.10

--- postman/tools/chocolateyInstall.ps1
+++ postman/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName= 'postman'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://dl.pstmn.io/download/version/10.18.4/win64'
+$url64      = 'https://dl.pstmn.io/download/version/10.18.10/win64'
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
   silentArgs    = "-s"
   url64bit      = $url64
-  checksum64    = '19eac3e57575a96e50f9b762d8970781e72786d49ecccbbe177c1c5c3e5bb6c6'
+  checksum64    = '0dee9e98e652018e279ada9dc905d64587fc13ffe915b747aac041d4ab78fec8'
   checksumType64= 'sha256'
 }
 
