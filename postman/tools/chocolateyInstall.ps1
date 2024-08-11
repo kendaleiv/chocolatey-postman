a869a3fa8d731f65725b88353e7f7651a8220b05
Dunigan <kalebdunigan@gmail.com>
2024-08-10 21:58:37 -0500

    Update chocolateyInstall.ps1 for 11.7.0

--- postman/tools/chocolateyInstall.ps1
+++ postman/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName= 'postman'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://dl.pstmn.io/download/version/11.1.0/win64'
+$url64      = 'https://dl.pstmn.io/download/version/11.7.0/win64'
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'exe'
   silentArgs    = "-s"
   url64bit      = $url64
-  checksum64    = '33401471665bc1d512659ee9362b5ef951e993fadecf1015f3fe03f9749b9384'
+  checksum64    = '0efec2748f38fdc65c1ef6141569f8006f197b058f7b1df8d0a4f0685eb8e42f'
   checksumType64= 'sha256'
 }
 
